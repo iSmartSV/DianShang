460830bd9619dc40c65a51b06d0b0f86867fbb7a
sv <405627142@qq.com>
2020-05-08 17:38:58 +0800

    # WARNING: head commit changed in the meantime
    aaaaaa

--- src/gitUpTest/test.java
+++ src/gitUpTest/test.java
@@ -4,6 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("Git Eclise Up Test");
+		System.out.println("Git Eclise Up Test2");
 		// TODO Auto-generated method stub
 
 	}
